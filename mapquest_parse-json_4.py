4e737b4a72f7c660032ee20775d7bdb921fb5342
sebastianduocuc <seb.valverde@duocuc.cl>
2023-05-17 01:46:14 +0000

    Consumode API Publica

---
+++ mapquest_parse-json_4.py
@@ -0,0 +1,22 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "PNi9xbk56Dtt47GyuyQEfLVZ7Fb1mqeN"
+
+while True:
+    orig = input("Starting Location: ")
+    if orig == "quit" or orig == "q":
+        break
+    dest = input("Destination: ")
+    if dest == "quit" or dest == "q":
+        break
+    url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest}) 
+    json_data = requests.get(url).json()
+
+    print("URL: " + (url))
+    json_status = json_data["info"]["statuscode"]
+
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
+
---
+++ mapquest_parse-json_5.py
@@ -0,0 +1,29 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = "PNi9xbk56Dtt47GyuyQEfLVZ7Fb1mqeN"
+
+while True:
+    orig = input("Starting Location: ")
+    if orig == "quit" or orig == "q":
+        break
+    dest = input("Destination: ")
+    if dest == "quit" or dest == "q":
+        break
+    url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest}) 
+    json_data = requests.get(url).json()
+
+    print("URL: " + (url))
+    json_status = json_data["info"]["statuscode"]
+
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
+        print("Directions from " + (orig) + " to " + (dest))
+        print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
+        print("Miles:           " + str(json_data["route"]["distance"]))
+        print("Kilometers:      " + str((json_data["route"]["distance"])*1.61))
+
+        
+
+
